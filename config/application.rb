8f97d43946674f2a5528e99b3eaa1e4139c2dc98
thinkAmi <dev.thinkami+git@gmail.com>
2022-06-09 21:47:42 +0900

    bin/rails app:update & bin/rails app:update:bin

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,10 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -31,8 +30,5 @@ module ReactWithViteRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
